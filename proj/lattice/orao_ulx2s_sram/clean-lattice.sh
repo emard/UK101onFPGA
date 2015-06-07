84edad89240ff9ad984060f0ab22aac1e64ad406
Emard <vordah@gmail.com>
2015-06-08 00:16:36 +0200

    ulx2s now using parametrized orao, get rid of old stuff

--- proj/lattice/orao_ulx2s_sram/clean-lattice.sh
+++ proj/lattice/orao_ulx2s_sram/clean-lattice.sh
@@ -13,10 +13,11 @@ SYMLINK="${SYMLINK} \
   ../../../lattice/pll_25M_50M.vhd \
   ../../../lattice/lattice_ProgRam.vhd \
   ../../../lattice/ProgRam.vhd \
-  ../../../lattice/main_orao.vhd \
-  ../../../orao/orao.vhd \
+  ../../../lattice/main_ulx2s_orao.vhd \
+  ../../../orao/orao_video_addr.vhd \
   ../../../generic/ROMgeneric.vhd \
   ../../../generic/rom_generic.v \
+  ../../../generic/bram_1port.vhd \
   ../../../generic/bram_2port.vhd \
   ../../../generic/ProgSRam.vhd \
   ../../../Components/M6502/T65_ALU.vhd \
