1fa6e7e8710a99901a1008cded82cfa02c770fff
Emard <vordah@gmail.com>
2015-06-11 10:32:33 +0200

    serial port mostly working, stream is 1 char late, many tx/rx errors

--- lattice/main_ulx2s_uk101_vga.v
+++ lattice/main_ulx2s_uk101_vga.v
@@ -79,7 +79,8 @@ uk101va
   .clk_mhz(25),  // MHz clock frequency
   .ram_kb(40),   // KB program RAM memory (up to 40 in steps of 4)
   .external_sram(1), // use external SRAM for program RAM
-  .cegmon("serial"), // use "serial" or "64x32" for cegmon
+  .cegmon("64x32"), // use "serial" or "64x32" for cegmon
+  .serial_baud(9600), // serial baud rate
   .model("101")  // UK101 model "101"
 )
 (
